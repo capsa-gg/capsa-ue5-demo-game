c6f87327d1192c458e7d341123b3c85b3157d0e0
Luciano Nooijen <luciano@lucianonooijen.com>
2025-02-19 23:01:15 +0100

    Initialize third person shooter from template

---
+++ Source/CapsaDemo/CapsaDemo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CapsaDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CapsaDemo, "CapsaDemo" );
+ 
